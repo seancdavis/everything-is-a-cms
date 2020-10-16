4305371ad106b54f4890dfe8b866cc79311bbbc5
Sean C Davis <scdavis41@gmail.com>
2020-10-16 09:57:37 -0400

    Add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import Head from "next/head"
+
 import "../styles/tailwind.css"
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Everything is a CMS!</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
